acb82e00282c9cbef178af36696fe5176c060959
Ethan <88164097+psyman5@users.noreply.github.com>
2024-02-19 11:30:34 -0500

    updated main.cpp for non-square matrices

--- main.cpp
+++ main.cpp
@@ -1,33 +1,25 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 #include <vector>
 
 
 int main(){
 
-    const int edgeSize{2};
-    int inputList[4] = {1,2,3,4};
+    const int rowSize{2};
+    const int columnSize{2};
+    int inputList[4] = {1,2,3,4}; // comma separated list of the values of the matrix from left to right and up to down. 
 
-    //std::cin >> num;
-    ///std::string numStr = std::to_string(num);
-    //int numLength = numStr.length();
+    const int rows{rowSize}; // computes the number of rows in the matrix based on given number of rows (i)
+    const int columns{columnSize}; // computes the number of columns in the matrix based on given number of columns (j)
 
-    const int rows{edgeSize};
-    const int columns{edgeSize};
-
-    int matrix[edgeSize][edgeSize] = {{0, 0}, {0, 0}};;
-
-    //for (char digit : numStr) {
-    //int currentDigit = digit - '0'; // Convert char to int
-    //}
-
-    int listCounter{0};
+    int matrix[rowSize][columnSize] = {{0, 0}, {0, 0}};
+    
+    int listCounter{0}; //counter that iterates through the list containing a row's columns.
 
     int row[rows] = {};
-    int rowCounter{0};
+    int rowCounter{0}; //counter that iterates through the lists in the array based on the size of the array, i in i,j  matrix format.
 
-    int col{0};
+    int col{0}; //column counter, same as j in ma
 
     while (listCounter < rows * columns){
        if(col < columns)
